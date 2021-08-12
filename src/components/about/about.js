9fd5a8435d9956fb6f69304dae3b999aa92e78ad
Rachit Agrawal <agrawal.rac@husky.neu.edu>
2021-08-12 12:37:29 -0400

    typos

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -5,7 +5,7 @@ export default class About extends Component {
   render() {
     return (
       <React.Fragment>
-       
+
       <section id="about">
         <div className="row">
           <div className="three columns">
@@ -14,11 +14,11 @@ export default class About extends Component {
           <div className="nine columns main-col">
             <h2>About Me</h2>
             <p>
-              Motivated, hard working and ingenous Software Engineer actively seeking full time opportunities from summer 2021, Graduate
+              Motivated, hard working and ingenious Software Engineer actively seeking full time opportunities from summer 2021, Graduate
                student pursuing Master of Science in Information Systems at Northeastern University with an aptitude for challenges
-                and a drive to efficiently manage workloads with high performance while also meeting stenous deadlines
+                and a drive to efficiently manage workloads with high performance while also meeting strenuous deadlines
             </p>
-          
+
           </div> {/* end .main-col */}
         </div>
       </section>
